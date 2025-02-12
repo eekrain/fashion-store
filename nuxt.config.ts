6790d28dbe17f22a64acb94801cbb8e018ed845c
Ardian Eka Candra <ardianoption@gmail.com>
2025-02-12 17:54:25 +0700

    init shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/color-mode",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/eslint",
+  ],
+  css: ["~/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
